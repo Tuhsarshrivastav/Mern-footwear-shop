a84be98c0e4bbd3e8b82d09d4966271736f4f65c
Tusharshrivastav <ts3657@gmail.com>
2021-11-17 16:42:00 +0530

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="App">
-    react
-    </div>
+    <Router>
+    <Switch>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/register' component={Register}/>
+      <Route exact path='/' component={Home}/>
+    </Switch>
+    </Router>
   );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Login = () => {
+const Home = () => {
     return (
         <div>
-            login
+            home
         </div>
     )
 }
 
-export default Login
+export default Home
