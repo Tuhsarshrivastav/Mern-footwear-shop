3aadf387ec10d2c1f61ecfcb343268b25403bbf5
Tusharshrivastav <ts3657@gmail.com>
2021-11-18 11:38:11 +0530

    ccomplete register system

--- client/src/pages/auth/RegisterComplete.jsx
+++ client/src/pages/auth/RegisterComplete.jsx
@@ -1,18 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { auth } from "../../firebase";
 import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
 const RegisterConplete = ({ history }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
   useEffect(() => {
     setEmail(window.localStorage.getItem("EmailforRegistration"));
   }, []);
-  
 
   const completeregisterform = () => {
     const handleSubmit = async (e) => {
       e.preventDefault();
+      if (!email && !password) {
+        toast.error("Email and Password is Required");
+        return;
+      }
+      if (password.length < 6) {
+        toast.error("Password must be 6 Characters");
+        return;
+      }
+      try {
+        const result = await auth.signInWithEmailLink(
+          email,
+          window.location.href
+        );
+        if (result.user.emailVerified) {
+          window.localStorage.removeItem("EmailforRegistration");
+          let user = auth.currentUser;
+          await user.updatePassword(password);
+          const idTokenResult = await user.getIdTokenResult();
+          console.log(idTokenResult, user);
+          //   history.push("/");
+        }
+      } catch (error) {
+        toast.error(error.message);
+      }
     };
     return (
       <form onSubmit={handleSubmit}>
@@ -37,6 +62,10 @@ const RegisterConplete = ({ history }) => {
         <div className="col-md-6 offset-md-3">
           <h4>Register Complete</h4>
           {completeregisterform()}
+
+          <Link to="/register">
+            <h5>Want to Chnage Email ?</h5>
+          </Link>
         </div>
       </div>
     </div>
