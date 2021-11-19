a016d81e747b12be0a8025393b637570f1da1622
Tusharshrivastav <ts3657@gmail.com>
2021-11-19 09:13:52 +0530

    added logout feature

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,29 @@
 import { Menu } from "antd";
 import {
   LoginOutlined,
+  LogoutOutlined,
   MailOutlined,
   SettingOutlined,
   UserAddOutlined,
 } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import firebase from "firebase";
+import { useDispatch } from "react-redux";
 const { SubMenu } = Menu;
+
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const logout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    history.push("/login");
+  };
   const [current, setCurrent] = useState("home");
   const handleClick = (e) => {
     setCurrent(e.key);
@@ -21,6 +36,9 @@ const Navbar = () => {
       <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
         <Menu.Item key="setting:1">Option 1</Menu.Item>
         <Menu.Item key="setting:2">Option 2</Menu.Item>
+        <Menu.Item icon={<LogoutOutlined />} key="setting:3" onClick={logout}>
+          Logout
+        </Menu.Item>
       </SubMenu>
       <Menu.Item
         key="register"
@@ -32,7 +50,7 @@ const Navbar = () => {
       <Menu.Item key="login" icon={<LoginOutlined />} className="float-right">
         <Link to="/login">Login</Link>
       </Menu.Item>
-    </Menu> 
+    </Menu>
   );
 };
 
